e00b8e5dec5092d7ab1b3b3a77ddc9987d504998
Jacob Bracey <jakebracey62@gmail.com>
2016-09-22 12:20:00 -0500

    starting the offset or scaling part

--- l4.c
+++ l4.c
@@ -18,9 +18,13 @@ int main(void) {
 	int* length=malloc(sizeof(int));
 	int* max_val=malloc(sizeof(int));
 	char file_name[16];
-	char stat_file[20];
+	char stat_file[23];
+	char offset_file[19];
+	char scaled_file[19];
 	int* array;
+	int off_or_scale;
 	
+	//gets file choice from user
 	printf("Which file would you like to open:\n");
 	scanf("%d",&file_sel);
 	if(file_sel<1||file_sel>11){
@@ -29,19 +33,39 @@ int main(void) {
 			scanf("%d", &file_sel);
 		}
 	}
+	
+	//actually creates the file names to be used
 	if(file_sel<10){
 	sprintf(file_name,"Raw_data_0%d.txt",file_sel);
-	sprintf(stat_file,"Statistics_data_0%d", file_sel);
+	sprintf(stat_file,"Statistics_data_0%d.txt", file_sel);
+	sprintf(offset_file,"Offset_data_0%d.txt", file_sel);
+	sprintf(scaled_file,"Scaled_data_0%d.txt", file_sel);
 	}
 	else{
 	sprintf(file_name,"Raw_data_%d.txt",file_sel);
-	sprintf(stat_file,"Statistics_data_%d", file_sel);	
+	sprintf(stat_file,"Statistics_data_%d.txt", file_sel);
+	sprintf(offset_file,"Offset_data_%d.txt", file_sel);
+	sprintf(scaled_file,"Scaled_data_%d.txt", file_sel);
 	}
 	
+	//loads the array from the input file
 	array=load_array(file_name, length, max_val);
-	printf("Would you like to\n1:offset the original signal\n2:")
+	
+	//part of code to either offset or scale
+	printf("Would you like to\n1:offset the original signal\n2:Scale the original signal\n");
+	scanf("%d",&off_or_scale);
+	if(off_or_scale<1||off_or_scale>11){
+		while(off_or_scale<1||off_or_scale>11){
+			printf("Invalid choice\nWould you like to\n1:offset the original signal\n2:Scale the original signal\n");
+			scanf("%d", &off_or_scale);
+		}
+	}
+	
+	
 	write_stats(stat_file, array, length); //writes stats to file
-}
+}//end of main
+
+
 int *load_array(char* file,int* length, int* max_val){
 	FILE* fp=fopen(file,"r");
 	//opens the given input file for reading
